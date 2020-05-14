e2ec73080f787250133fa27ffeea4db5b713a208
microsweet <599058045@qq.com>
2020-05-14 11:16:14 +0800

    notify

---
+++ go.mod
@@ -0,0 +1,10 @@
+module notify
+
+go 1.14
+
+require (
+	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4
+	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+)
---
+++ notify.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/0xAX/notificator"
+)
+
+var notify *notificator.Notificator
+
+func main() {
+	goos := runtime.GOOS
+
+	for {
+		time.Sleep(30 * time.Minute)
+		now := time.Now().Format("15:04")
+		nowPrev, _ := strconv.Atoi(strings.Split(now, ":")[0])
+		nowNex, _ := strconv.Atoi(strings.Split(now, ":")[1])
+		fmt.Println(goos)
+		if nowPrev == 17 && nowNex >= 30 {
+			continue
+		} else if nowPrev > 17 {
+			continue
+		} else if nowPrev == 8 && nowNex <= 30 {
+			continue
+		} else if nowPrev < 8 {
+			continue
+		}
+
+		notify = notificator.New(notificator.Options{
+			DefaultIcon: "icon/default.png",
+			AppName:     "my test app",
+		})
+		pngPath, _ := os.Getwd()
+		pngPath = pngPath + "/appointment-soon.png"
+		notify.Push("现在时间"+now+"，休息一下吧", "", pngPath, notificator.UR_CRITICAL)
+	}
+}
